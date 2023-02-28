516a1f039c36e9d897eb206fdc242d0d519ad3d9
rifatrakib <Rakib.1508@outlook.com>
2023-02-28 09:04:21 +0600

    fix issues with installation

--- models/setup.py
+++ models/setup.py
@@ -1,3 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name="pydantic-mongo-reusables",
+    version="0.1",
+    description="A collection of Pydantic models that can be reused and extended by anything that are built on top of pydantic.",
+    url="https://www.example.com/",
+    author="Md Abdur Rakib",
+    author_email="<Rakib.1508@outlook.com>",
+    packages=find_packages(),
+    install_requires=[
+        "pydantic >= 1.10.5",
+        "python-dotenv >= 0.10.4",
+        "email-validator >= 1.0.3",
+        "pydash >= 6.0.2",
+        "python-decouple >= 3.7",
+    ],
+    keywords=["python"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
