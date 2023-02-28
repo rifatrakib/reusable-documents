1d2d8fd54869b680c8fa86bde350cf9fbfde7e3c
rifatrakib <Rakib.1508@outlook.com>
2023-02-28 09:09:29 +0600

    formatted version of setup script

--- models/setup.py
+++ models/setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="pydantic-mongo-reusables",
     version="0.1",
-    description="A collection of Pydantic models that can be reused and extended by anything that are built on top of pydantic.",
+    description=(
+        "A collection of Pydantic models that can be reused and extended by anything that are built on top of pydantic."
+    ),
     url="https://www.example.com/",
     author="Md Abdur Rakib",
     author_email="<Rakib.1508@outlook.com>",
